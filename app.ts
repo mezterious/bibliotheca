06ad03bb86b824aafa7fdf887b98bdbd55c7d257
Mezterious <mezteriousone@gmail.com>
2018-09-24 18:20:29 +1000

    Add cors middleware

--- app.ts
+++ app.ts
@@ -1,12 +1,25 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './app/routes';
 
 const app = express();
 
+// Add middleware
+app.use(
+  cors({
+    origin: ['http://localhost:3001'],
+    methods: ['POST', 'GET'],
+    allowedHeaders: ['Content-Type'],
+  }),
+);
+
+// Mount routes
 app.use('/api', router);
 
 app.get('/', (_, res) => {
-  res.send("Awesome! We're live debugging this!");
+  res.json({
+    message: "Awesome! We're live debugging this!",
+  });
 });
 
 export { app };
