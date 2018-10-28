e5a598864e45724e1f6d1e3b34b0d0b2f93f562c
Mezterious <mezteriousone@gmail.com>
2018-10-28 13:49:43 +1100

    Add open API specification and validation

---
+++ app/middlewares/validator.ts
@@ -0,0 +1,9 @@
+import { readFileSync } from 'fs';
+import { OpenApiValidator } from 'express-openapi-validate';
+import jsYaml from 'js-yaml';
+
+const openApiDoc = jsYaml.safeLoad(readFileSync('api-spec.yml', 'utf-8'));
+
+const validator = new OpenApiValidator(openApiDoc);
+
+export { validator };
--- app/routes/v1/books.ts
+++ app/routes/v1/books.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import { booksController } from '../../controllers/v1/books';
 import { asyncHandler } from '../../middlewares/asyncHandler';
+import { validator } from '../../middlewares/validator';
 
 const router = express.Router();
 
-router.post('/', asyncHandler(booksController.createBook));
+router.post(
+  '/',
+  validator.validate('post', '/books'),
+  asyncHandler(booksController.createBook),
+);
 
 export { router as booksRouter };
